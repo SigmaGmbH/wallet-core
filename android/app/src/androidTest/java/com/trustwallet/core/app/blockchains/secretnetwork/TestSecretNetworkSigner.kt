f739895cfbf4b132bc08095d6039fd2320508303
splend1d_man <splend1d_man@sgm.team>
2022-09-08 16:36:38 +0300

    Try fix ios & android tests

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/secretnetwork/TestSecretNetworkSigner.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/secretnetwork/TestSecretNetworkSigner.kt
@@ -35,7 +35,7 @@ class TestOsmosisSigner {
         val from = AnyAddress(publicKey, SECRETNETWORK).description()
 
         val txAmount = Cosmos.Amount.newBuilder().apply {
-            amount = 100000 - 200
+            amount = "99800"
             denom = "uscrt"
         }.build()
 
@@ -50,7 +50,7 @@ class TestOsmosisSigner {
         }.build()
 
         val feeAmount = Cosmos.Amount.newBuilder().apply {
-            amount = 200
+            amount = "200"
             denom = "uscrt"
         }.build()
 
@@ -72,7 +72,7 @@ class TestOsmosisSigner {
 
         val output = AnySigner.sign(signingInput, SECRETNETWORK, SigningOutput.parser())
 
-        assertEquals(output.serialized, "CpEBCo4BChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEm4KLXNlY3JldDFuY3Y0eDd3MDVrN3RzeDhlOTZhYzZ6ZHo4MGx3dmxod2V1ZGduYxItc2VjcmV0MTlrcDB4Y215OGdmczM0ZmN6emg0ZTYzZ2QzdTg5Y3NwdGNuNzhsGg4KBXVzY3J0EgU5OTgwMBJkCk4KRgofL2Nvc21vcy5jcnlwdG8uc2VjcDI1NmsxLlB1YktleRIjCiEC5ybzmDF8f/3D+nJghFSvhOMVx7wEA0rE1k4mScabRnoSBAoCCAESEgoMCgV1c2NydBIDMjAwEMCaDBpAOIbYTCecUVmuUOsBh1Cx99orfJsM3A0XQBCIzE6F1ixM8fPIycHUBGyCnoMgLGJQtCYj+yo9bokhba89IuQU/Q==")
+        assertEquals(output.serialized, "{\"mode\":\"BROADCAST_MODE_BLOCK\",\"tx_bytes\":\"CpEBCo4BChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEm4KLXNlY3JldDFuY3Y0eDd3MDVrN3RzeDhlOTZhYzZ6ZHo4MGx3dmxod2V1ZGduYxItc2VjcmV0MTlrcDB4Y215OGdmczM0ZmN6emg0ZTYzZ2QzdTg5Y3NwdGNuNzhsGg4KBXVzY3J0EgU5OTgwMBJkCk4KRgofL2Nvc21vcy5jcnlwdG8uc2VjcDI1NmsxLlB1YktleRIjCiEC5ybzmDF8f/3D+nJghFSvhOMVx7wEA0rE1k4mScabRnoSBAoCCAESEgoMCgV1c2NydBIDMjAwEMCaDBpAOIbYTCecUVmuUOsBh1Cx99orfJsM3A0XQBCIzE6F1ixM8fPIycHUBGyCnoMgLGJQtCYj+yo9bokhba89IuQU/Q==\"}")
         assertEquals(output.error, "")
     }
 }
--- swift/Tests/Blockchains/SecretNetworkTests.swift
+++ swift/Tests/Blockchains/SecretNetworkTests.swift
@@ -14,7 +14,7 @@ class SecretNetworkTests: XCTestCase {
         let address = AnyAddress(publicKey: pubkey, coin: .secretNetwork)
         let addressFromString = AnyAddress(string: "secret1kc3629yhu93s8c3gv3sylzdtr4fqzmgqxexc2z", coin: .secretNetwork)!
 
-        XCTAssertEqual(pubkey.data.hexString, "032f0c8bd1cf6cdaa754cfa85edc45105a2b0492a465f150fb25e5ab97ff8c709a")
+        XCTAssertEqual(pubkey.data.hexString, "02ecef5ce437a302c67f95468de4b31f36e911f467d7e6a52b41c1e13e1d563649")
         XCTAssertEqual(address.description, addressFromString.description)
     }
 
@@ -27,7 +27,7 @@ class SecretNetworkTests: XCTestCase {
             $0.fromAddress = fromAddress.description
             $0.toAddress = "secret19kp0xcmy8gfs34fczzh4e63gd3u89csptcn78l"
             $0.amounts = [CosmosAmount.with {
-                $0.amount = 100000 - 200
+                $0.amount = "99800"
                 $0.denom = "uscrt"
             }]
         }
@@ -39,7 +39,7 @@ class SecretNetworkTests: XCTestCase {
         let fee = CosmosFee.with {
             $0.gas = 200000
             $0.amounts = [CosmosAmount.with {
-                $0.amount = 200
+                $0.amount = "200"
                 $0.denom = "uscrt"
             }]
         }
@@ -57,7 +57,7 @@ class SecretNetworkTests: XCTestCase {
 
         let output: CosmosSigningOutput = AnySigner.sign(input: input, coin: .secretNetwork)
 
-        XCTAssertEqual(output.serialized, "CpEBCo4BChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEm4KLXNlY3JldDFuY3Y0eDd3MDVrN3RzeDhlOTZhYzZ6ZHo4MGx3dmxod2V1ZGduYxItc2VjcmV0MTlrcDB4Y215OGdmczM0ZmN6emg0ZTYzZ2QzdTg5Y3NwdGNuNzhsGg4KBXVzY3J0EgU5OTgwMBJkCk4KRgofL2Nvc21vcy5jcnlwdG8uc2VjcDI1NmsxLlB1YktleRIjCiEC5ybzmDF8f/3D+nJghFSvhOMVx7wEA0rE1k4mScabRnoSBAoCCAESEgoMCgV1c2NydBIDMjAwEMCaDBpAOIbYTCecUVmuUOsBh1Cx99orfJsM3A0XQBCIzE6F1ixM8fPIycHUBGyCnoMgLGJQtCYj+yo9bokhba89IuQU/Q==")
+        XCTAssertJSONEqual(output.serialized, "{\"mode\":\"BROADCAST_MODE_BLOCK\",\"tx_bytes\":\"CpEBCo4BChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEm4KLXNlY3JldDFuY3Y0eDd3MDVrN3RzeDhlOTZhYzZ6ZHo4MGx3dmxod2V1ZGduYxItc2VjcmV0MTlrcDB4Y215OGdmczM0ZmN6emg0ZTYzZ2QzdTg5Y3NwdGNuNzhsGg4KBXVzY3J0EgU5OTgwMBJkCk4KRgofL2Nvc21vcy5jcnlwdG8uc2VjcDI1NmsxLlB1YktleRIjCiEC5ybzmDF8f/3D+nJghFSvhOMVx7wEA0rE1k4mScabRnoSBAoCCAESEgoMCgV1c2NydBIDMjAwEMCaDBpAOIbYTCecUVmuUOsBh1Cx99orfJsM3A0XQBCIzE6F1ixM8fPIycHUBGyCnoMgLGJQtCYj+yo9bokhba89IuQU/Q==\"}")
         XCTAssertEqual(output.error, "")
     }
 }
